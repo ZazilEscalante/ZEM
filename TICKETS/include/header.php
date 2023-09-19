75b851f0c2c808e593bae72e14559a0a1c3d32ec
Zazil Escalante <zem7410@hotmail.com>
2023-09-18 18:24:38 -0600

    hello

---
+++ TICKETS/include/header.php
@@ -0,0 +1,8 @@
+<nav class="navbar bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.es%2Ffotos-vectores-gratis%2Flogo-png&psig=AOvVaw2G0dkjn-pFhd_-RuL6t2UT&ust=1695158067337000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMCq94WKtYEDFQAAAAAdAAAAABAE" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Sistema de tickets
+    </a>
+  </div>
+</nav>
